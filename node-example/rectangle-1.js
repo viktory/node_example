dd8ea98cdff5d4c7836f2fd31460ffee0a15fe69
Viktory <you@example.com>
2016-10-18 22:08:45 +0300

    week1

---
+++ node-example/rectangle-1.js
@@ -0,0 +1,12 @@
+/**
+ * Date: 14.10.16
+ * @author Viktoriia Lysenko <lysenkoviktory@gmail.com>
+ */
+
+exports.perimeter = function (x, y) {
+    return (2*(x+y));
+}
+
+exports.area = function (x, y) {
+    return (x*y);
+}
